d75b6d273ee7839270649a9c089117f6ece44d86
coder-rabbani <thegolamrabbani@gmail.com>
2021-10-29 11:46:43 +0600

    final serve3r

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // step 1
 const express = require('express');
 
+//require ObjectId
+const ObjectId = require('mongodb').ObjectId;
+
+//import cors
+const cors = require('cors');
+
 //mongodb import
 const { MongoClient } = require('mongodb');
+
 //dotenv package requirement for environment variable for database secret info like user and pass
 require('dotenv').config();
 
@@ -11,6 +18,10 @@ const app = express();
 //step 3
 const port = 5000;
 
+//Middleware 
+app.use(cors());
+app.use(express.json())
+
 // MongoDB settings/connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v05ft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,13 +29,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
   try{
     await client.connect();
+    const database = client.db('geniusCarMechanic');
+    const servicesCollection = database.collection('services');
+
+    //get api
+    app.get('/services', async(req, res)=>{
+      const cursor = servicesCollection.find({}); 
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    //GET single service
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const service = await servicesCollection.findOne(query);
+      res.json(service)
+    })
+
+    //post api
+    app.post('/services', async(req, res)=>{
+      const service = req.body;
+      console.log('hit the post api ', service);
+      
+      const result = await servicesCollection.insertOne(service);
+      console.log(result); 
+      res.json(result);
+    })
+
+    //DELETE api
+    app.delete('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    })
   }
-  finally{
+  finally{ 
     // await client.close();
   }
 }
 
-run().catch(console.dir);
+run().catch(console.dir); 
 
 
 //step 4 - defalut route
